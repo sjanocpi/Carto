3e863178d244a1bee2d1c8345221fa0d6a8d275b
Stéphane Jeanneau <git@sjanocpi.fr>
2020-06-30 13:24:09 +0200

    Modifications fichiers, ajouts commentaires, améliorations code

--- deplacer-json-Nord-Sud-Est-Ouest.php
+++ deplacer-json-Nord-Sud-Est-Ouest.php
@@ -7,6 +7,15 @@
 </head>
 <body>
 
+<!--  Ce document sert à déplacer les coordonnées des territoires outre-marins à coté de la métropole afin d'avoir une carte centrale.
+      Il va récupérer un fichier au format json et en faire une copie avec les nouvelles valeurs.
+      Ce fichier Json pourrra être utilisé comme source de données dans une carte.
+      Les valeurs sont à entrer à la mano, tout comme le nom du json/geojson original.
+      C'est dans la fonction numCallBack que les valeurs se changent, en fonction de l'hémisphère les valeus seront plus ou moins importantes,
+      négatives ou positives.
+      Voir commentaires plus bas dans le code.
+-->
+
 <!-- pre c'est plus simple pour la mise en page -->
 <pre style="white-space: break-spaces;">
 
@@ -15,21 +24,27 @@
   </form>
 
 <?php
+// -- Fichier à placer en locahost
+
 // Reset/Reload
 echo '<h3><a href="'.$_SERVER['REQUEST_URI'].'">Reset/Reload</a></h3><hr>';
 
-// endroit où générer le fichier, www-data doit avoir les droits sur ce dossier
+// Dossier où générer le fichier, www-data doit avoir les droits sur ce dossier
 $dossier = "ecritures";
 
 //  ------------ Mettre à jour ce fichier ------------
-$dataName = "976";
-$extension = ".json";
+// format data du type geojson, pas d 'espaces entre les accolades ou les virgules :
+// "geometry":{"type":"Polygon","coordinates":[[[-61.163143,14.815349],[-61.162844,14.815126] ...
 
+$dataName = "976";
+//  json geosjson etc.
+$extension = ".json";
+// On concatène les 2 pour avoir le nom complet, par ex. 976.json
 $name = $dataName.$extension;
-$lines = file("$name");
+//
+$dataLines = file("$name");
 // pour écrire dans celui-ci
 $creaName = $dataName."-move".$extension;
-
 
 
 /*  ------------ Pour le calcul dans la fonction numCallBack ------------
@@ -58,8 +73,10 @@ $creaName = $dataName."-move".$extension;
   $lat = $matches[3] + 31.4;
   pour à-peu-près pareil
 
-  Les fichiers json peuvent être testés ici :
+
+  Le placement des données du fichiers json peut être testé ici :
   http://geojson.io/#map=2/20.0/0.0
+
   le regex ici
   https://regex101.com/
 */
@@ -74,23 +91,15 @@ function numCallBack($matches) {
 }
 
 if(!empty($_POST['envoi'])) {
-// if(!empty($_POST['envoi']) && !empty($_POST['choix']) ) {
-
-  // if ($_POST['choix'] == 'sud') {
-  //   $preg = array("/[-+]?([0-9]*\.[0-9]*),([-+]?([0-9]*\.[0-9]*))/");
-  // }else {
 
   $preg = array("/([-+]?([0-9]*\.[0-9]*)),([-+]?([0-9]*\.[0-9]*))/");
 
-  // }
 
-  $newData = preg_replace_callback($preg, "numCallBack", $lines);
-  // print_r($newData);
+  $newData = preg_replace_callback($preg, "numCallBack", $dataLines);
 
   $fp = fopen($dossier.'/'.$creaName, 'w');
 
-  // echo '<h3><a href="'.$_SERVER['REQUEST_URI'].'">Reset</a></h3><hr>';
-
+  // on loop et
   foreach ($newData as $key => $value) {
     // on écrit les données
     fwrite($fp, $value);
@@ -101,7 +110,7 @@ if(!empty($_POST['envoi'])) {
   fclose($fp);
 
 } else {
-
+  // mode par défaut
   echo "Éditer ce fichier pour son fonctionnement avant de valider,
   le fichier json est : <b>$name</b>
   Il sera généré en tant que <b>$creaName</b>
